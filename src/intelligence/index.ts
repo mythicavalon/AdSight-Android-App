6d1d29c44ea08d6830ac008d499ab438883f10ae
avalon <amalrnair1998@gmail.com>
2026-09-14 13:54:41 +0530

    feat: expose V2 intelligence module

---
+++ src/intelligence/index.ts
@@ -0,0 +1,9 @@
+export { InferenceEngineV2 } from './InferenceEngineV2';
+export { clamp01, normalizedStrength, recencyMultiplier } from './Recency';
+export type {
+  EvidenceItem,
+  InferenceResult,
+  InferenceSignal,
+  InferenceSnapshot,
+  SignalType,
+} from './v2Types';
